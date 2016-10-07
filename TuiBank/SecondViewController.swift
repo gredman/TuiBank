228ad52cc7357c9d52d48dff0825982324a8fccc
Gareth Redman <gredman@nil.gen.nz>
2016-10-08 10:26:47 +1300

    Initial Commit

---
+++ TuiBank/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TuiBank
+//
+//  Created by Gareth Redman on 8/10/16.
+//  Copyright © 2016 Gareth Redman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
